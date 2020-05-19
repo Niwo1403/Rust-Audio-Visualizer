85efc45b0b783b2fddd26701634507be31c047ff
Cedric00 <cedric.ressler0049@gmail.com>
2020-05-19 18:07:52 +0200

    Added Frame+Run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 
 extern crate glium;
 
+use glium::Surface;
+
 fn main() {
     println!("Hello, world!");
+
+    use glium::glutin;
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+
+        //----------------draw something--------------------------
+
+        let mut target = display.draw();
+        target.clear_color(1.0, 1.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        //---------------end drawing------------------------------
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
+
+
+
+
 }
