c59f2f300767187f533eec743dabb9cc598eb944
Cedric00 <cedric.ressler0049@gmail.com>
2020-05-26 18:04:08 +0200

    Simple Drawing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+#[macro_use]
 extern crate glium;
 
 use glium::Surface;
@@ -12,14 +12,52 @@ use glium::Surface;
 
 
 fn main() {
-    println!("Hello, world!");
-
-    use glium::glutin;
+    use glium::{glutin, Surface};
 
     let mut event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    //---------------------------------new copy-------------------------------
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    //------------------------------------------------------------------------
+
+
+
+
+
 
     event_loop.run(move |ev, _, control_flow| {
         let next_frame_time = std::time::Instant::now() +
@@ -29,8 +67,9 @@ fn main() {
 
         let mut target = display.draw();
         target.clear_color(1.0, 0.7, 0.4, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
         target.finish().unwrap();
-
+        print!("Drawing");
         //---------------end drawing------------------------------
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         match ev {
@@ -44,8 +83,5 @@ fn main() {
             _ => (),
         }
     });
-
-
-//Hier ein Kommentar
 
 }
