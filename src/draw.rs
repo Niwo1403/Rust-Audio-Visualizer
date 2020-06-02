20f0d0b46d236f62d7bb9298374d64da89d6aa4f
Cedric00 <cedric.ressler0049@gmail.com>
2020-06-02 18:18:32 +0200

    Simple Drawing

---
+++ src/draw.rs
@@ -0,0 +1,48 @@
+
+use glium::{glutin, Surface, Frame, Display};
+
+
+pub fn draw(mut target: Frame,  display: &Display) {
+
+    //---------------------------------new copy-------------------------------
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    //------------------------------------------------------------------------
+
+
+
+    target.clear_color(1.0, 0.7, 0.4, 1.0);
+    target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+    target.finish().unwrap();
+
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use glium::Surface;
 use std::process::exit;
 
 
+mod window;
+mod draw;
+
 //TODO Fileselector
-//TODO UI - Knopf
 //TODO Malen
 //TODO Audiodatein lesen
 //TODO FFT-Bibliotheken benutzen
@@ -30,77 +32,7 @@ fn main() {
     }
     println!("Filename: {:?}", args[1]);
 
-    //Rest
-    use glium::{glutin, Surface};
-
-    let mut event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-    //---------------------------------new copy-------------------------------
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position);
-
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let vertex_shader_src = r#"
-        #version 140
-        in vec2 position;
-        void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-        out vec4 color;
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-    //------------------------------------------------------------------------
-
-
-
-
-
-
-    event_loop.run(move |ev, _, control_flow| {
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
-
-        //----------------draw something--------------------------
-
-        let mut target = display.draw();
-        target.clear_color(1.0, 0.7, 0.4, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-        target.finish().unwrap();
-        print!("Drawing");
-        //---------------end drawing------------------------------
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-        match ev {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                },
-                _ => return,
-            },
-            _ => (),
-        }
-    });
+    //Starte Fenster
+    window::openWindow();
 
 }
