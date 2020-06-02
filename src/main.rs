49b344ca3db38bbf81fdb90081b210e8ddf9e11d
Niwo1493 <nicolai-wolfrom@gmx.de>
2020-06-02 18:07:32 +0200

    Getting terminal argument & printing help menu.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate glium;
 
 use glium::Surface;
+use std::process::exit;
 
 
 //TODO Fileselector
@@ -12,6 +13,24 @@ use glium::Surface;
 
 
 fn main() {
+    // Argumente
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    // aud -h testen und ggf. Hilfemenu ausgeben
+    for arg in &args {
+        if *arg == String::from("-h") {
+            println!("Aufruf: ausiovisualizer.exe <DATEINAME> <OPTION>\n\tDATEINAME - der Name der Audiodatei\n\tOPTION - zusätzliche Einstellungsmöglichkeit\n");
+            exit(0);
+        }
+    }
+    // Überprüfen ob Dateiname vorhanden ist
+    if args.len() < 2 {
+        println!("Bitte den Dateinamen der Audiodatei als Argument übergeben.");
+        exit(1);
+    }
+    println!("Filename: {:?}", args[1]);
+
+    //Rest
     use glium::{glutin, Surface};
 
     let mut event_loop = glutin::event_loop::EventLoop::new();
