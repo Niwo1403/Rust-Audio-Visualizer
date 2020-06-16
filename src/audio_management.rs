adf4210cb1262256ca883dd37f787dca34ce46d0
Pirin <pirin_marinov@abv.bg>
2020-06-16 17:00:33 +0200

    Edited

--- src/audio_management.rs
+++ src/audio_management.rs
@@ -1,17 +1,3 @@
-//The main concept of this library is the Source trait, which represents a sound (streaming or not). In order to play a sound, there are three steps:
-//to represent a sound in memory we need three characteristics: the frequency, the number of channels,
-// and the list of samples
-
-// Rust object that represents a sound should implement the Source trait.
-// The three characteristics that describe a sound are provided through this trait:
-//
-// The number of channels can be retrieved with channels.
-// The frequency can be retrieved with sample_rate.
-// The list of values can be retrieved by iterating on the source. The Source trait requires that the Iterator trait be implemented as well.
-
-// Create an object that represents the streaming sound. It can be a sine wave, a buffer, a decoder, etc. or even your own type that implements the Source trait.
-// Choose an output with the devices or default_output_device functions.
-// Call play_raw(output, source).
 //Source: https://docs.rs/rodio/0.11.0/rodio/
 
 use std::fs::File;
