3ba48da01f528ce4a7f5e66580d2cd1c1108e407
Pirin <pirin_marinov@abv.bg>
2020-06-23 17:00:27 +0200

    added read function

--- src/audio_management.rs
+++ src/audio_management.rs
@@ -2,10 +2,9 @@
 
 use std::fs::File;
 use std::io::BufReader;
-//Rust object that represents a sound should implement the Source trait.
-//use rodio::Source;
-//type Sink controls  playback (represents audio track).
-use rodio::Sink;
+//use rodio::Source;    //Rust object that represents a sound should implement the Source trait.
+use rodio::Sink;       //type Sink controls  playback (represents audio track).
+use std::fs::File;     //to read bytes of file
 
 /*to play a sound:
 - Create an object that represents the streaming sound. It can be a sine wave, a buffer, a decoder.
@@ -23,3 +22,11 @@ pub fn play_audio(arg: &str) {
 
     sink.append(source);
 }
+
+
+let mut f = File::open(arg)?;
+let mut buffer = Vec::new();
+// Read all bytes until EOF in this source, placing them into buf.
+// All bytes read from this source will be appended to the specified buffer buf.This function will continuously call read() to append more data to buf until read() returns either Ok(0) or an error of non-ErrorKind::Interrupted kind.
+// If successful, this function will return the total number of bytes read.
+f.read_to_end(&mut buffer)?;
